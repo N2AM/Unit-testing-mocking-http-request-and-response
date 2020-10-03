268b3192d5321d61b38ee448d532f7925ea71a7e
Nouran Mahmoud Abdel Latif Moursy elRefai <Nouran.ElRefai@Egypt.TE-Data.core>
2020-10-03 11:28:26 +0200

    creating js structured project files to be tested

---
+++ src/services/unsplash.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+export default async (term) => {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    params: {
+      client_id: process.env.REACT_APP_UNSPLASH_TOKEN,
+      query: term,
+    },
+  });
+};
